c40efe762358635bb9ec790aee4b3ac01f56a655
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-10-08 20:02:27 +0200

    Testing  to decide on page layout

---
+++ MusicStore/Controllers/MusicController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+    public class MusicController : Controller
+    {
+        // GET: Music
+        public ActionResult Random()
+        {
+            var song = new Song() { name = "This Fffire" };
+
+
+            return View(song);
+
+
+        }
+
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("1");
+        }
+
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue) pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy)) sortBy = "Name";
+
+            return Content(String.Format("Page index={0} & sortBy={1}", pageIndex, sortBy));
+        }
+
+    }
+
+
+}
